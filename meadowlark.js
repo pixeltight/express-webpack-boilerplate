da101dc69d6ab6cce30fe9a7ef808bc47ffe26fc
pixeltight <jkerr013@gmail.com>
2016-12-01 23:07:59 -0500

    flesh out templates and views

--- meadowlark.js
+++ meadowlark.js
@@ -2,27 +2,37 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.set('port', process.env.PORT || 3000);
+const handlebars = require('express-handlebars')
+				   .create({defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {title: 'About My Shop of Horrors'});	
+});
 
 // custom 404
-app.use((req, res) => {
-	res.type('text/plain');
-	res.status('404');
-	res.send('404 - Resource Not Found');
+app.use((req, res, next) => {
+	res.status(404);
+	res.render('404');
 });
-
 
 // custom 500
-app.use((err, req, res, next) =>  {
+app.use((err, req, res, next) => {
 	console.error(err.stack);
-
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Internal Server Error');
+	res.render('505');
 });
 
-app.listen(app.get('port'), () => {
-	console.log(`Express started on http://localhost: {app.get('port')};
-		Press Ctrl-C to terminate`);	
+app.listen(port, () => {
+	console.log(`listening on port ${port}`);
 });
