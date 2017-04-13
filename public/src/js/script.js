894b8bd33f746da059483faa44d65dbedf8fc62c
Jason Kerr <jkerr013@gmail.com>
2017-04-12 23:31:41 -0400

    building repo for aws

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,3 +1,4 @@
+import '../../../node_modules/bootstrap/scss/bootstrap.scss'
 import '../css/style.scss'
 
 alert('i hate my life')
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const glob = require('glob')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const PurifyCssPlugin = require('purifycss-webpack')
 
 module.exports = {
   context: path.resolve(__dirname, 'public/src'),
@@ -18,8 +21,33 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => ([
+                  require('autoprefixer')
+                ])
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                precision: 8
+              }
+            }
+          ]
+        })
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new PurifyCssPlugin({
+      paths: glob.sync(path.join(__dirname, 'src/*.html'))
+    })
+  ]
 }
