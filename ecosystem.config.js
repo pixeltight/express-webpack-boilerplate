c9514ae6d7fb35641c6496c4b0eb45440b7f2b4b
Jason Kerr <jkerr013@gmail.com>
2017-04-13 01:40:14 -0400

    trying to set up new server

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'express-webpack-boilerplate',
+    script: './app.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-52-14-208-203.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/aws-test.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:pixeltight/express-webpack-boilerplate.git',
+      path: '/home/ubuntu/express-webpack-boilerplate',
+      'post-deploy': 'yarn && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
