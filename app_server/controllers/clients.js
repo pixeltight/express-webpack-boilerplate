7a65d3eb756f3804f24f72f3646df69cb3aaea26
Jason Kerr <jkerr013@gmail.com>
2017-04-18 12:39:17 -0400

    force string to integer after === in router, functions to es6 arrow functions

--- app_server/controllers/clients.js
+++ app_server/controllers/clients.js
@@ -1,19 +1,19 @@
 const clients = require('../models/clients.json')
 
 /* GET 'home' page */
-module.exports.clientList = function (req, res) {
+module.exports.clientList = (req, res) => {
   res.render('clientList', {
     title: 'Jason Kerr Homepage',
     clients: clients
   })
 }
 
-module.exports.clientInfo = function (req, res) {
-  var arrIndex = req.params.id - 1
+module.exports.clientInfo = (req, res) => {
+  let arrIndex = req.params.id - 1
   res.render('clientInfo', {
     title: 'Clients: ' + clients[arrIndex].name,
-    client: clients.find(function (key) {
-      return key.id === req.params.id
+    client: clients.find(key => {
+      return key.id === +req.params.id
     })
   })
 }
