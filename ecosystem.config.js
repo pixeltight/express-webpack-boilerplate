e30f76c41b9c9e02e46915f5cb337578bdf79160
Jason Kerr <jkerr013@gmail.com>
2017-10-21 17:19:42 -0400

    change to ecosystem.config after elastic IP change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,12 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-15-148-249.us-east-2.compute.amazonaws.com',
+      host: 'ec2-18-216-84-6.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/aws-test.pem',
       ref: 'origin/master',
       repo: 'git@github.com:pixeltight/express-webpack-boilerplate.git',
       path: '/home/ubuntu/express-webpack-boilerplate',
-      'post-deploy': 'pm2 startOrRestart ecosystem.config.js'
+      'post-deploy': 'yarn && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }
