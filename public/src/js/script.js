f9552f25269ea910f5deaf4abdb3fd82a05ad74e
Jason Kerr <jkerr013@gmail.com>
2017-11-20 01:40:35 -0500

    js bug in ios/safari

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,53 +1,58 @@
 import '../scss/style.scss'
 
-// flyout menu
-let hamburger = document.getElementById('burgerToggle')
-let flyoutMenu = document.getElementById('flyoutMenu')
-let indexdots = document.getElementById('indexdots')
-let portfolio = document.getElementById('portfolio')
-let portfolioLink = document.getElementById('portfolioLink')
-
-function showMenu () {
-  flyoutMenu.classList.add('show')
-  document.body.style.overflow = 'hidden'
+window.onpageshow = (e) => {
+  if (e.persisted) {
+      window.location.reload() 
+  }
 }
 
-function hideMenu () {
-  flyoutMenu.classList.remove('show')
-  document.body.style.overflow = 'auto'
-}
+;(function () {
+  let indexdots = document.getElementById('indexdots')
+  if (indexdots) {
+    let indexLinks = document.querySelectorAll('a.indexdots__link')
+    let indexLink = (document.getElementById('index').value) - 1
+    let activeLink = indexLinks[indexLink]
+    activeLink.classList.add('active')
+  }
+})()
 
-if (hamburger) {
-  hamburger.addEventListener('click', () => {
-    if (!hamburger.classList.contains('is-active')) {
-      hamburger.classList.add('is-active')
-      showMenu()
-    } else {
+;(function () {
+  const showMenu = () => {
+    document.getElementById('flyoutMenu').classList.add('show')
+    document.body.style.overflow = 'hidden'
+  }
+
+  const hideMenu = () => {
+    document.getElementById('flyoutMenu').classList.remove('show')
+    document.body.style.overflow = 'auto'
+  }
+
+  let portfolio = document.getElementById('portfolio')
+  let portfolioLink = document.getElementById('portfolioLink')
+  let hamburger = document.getElementById('burgerToggle')
+
+  if (hamburger) {
+    hamburger.addEventListener('click', () => {
+      if (!hamburger.classList.contains('is-active')) {
+        hamburger.classList.add('is-active')
+        showMenu()
+      } else {
+        hamburger.classList.remove('is-active')
+        hideMenu()
+      }
+    }, false)
+  }
+
+  if (portfolio) {
+    portfolioLink.addEventListener('click', () => {
       hamburger.classList.remove('is-active')
       hideMenu()
-    }
-  }, false)
-}
-
-// if hitting hashed link from homepage
-// close mobile nav
-
-if (portfolio) {
-  portfolioLink.addEventListener('click', () => {
-    hamburger.classList.remove('is-active')
-    hideMenu()
-  }, false)
-}
-
-if (indexdots) {
-  let indexLinks = document.querySelectorAll('a.indexdots__link')
-  let indexLink = (document.getElementById('index').value) - 1
-  let activeLink = indexLinks[indexLink]
-  activeLink.classList.add('active')
-}
+    }, false)
+  }
+})()
 
 // send mail if validated
-(function ($) {
+;(function ($) {
   'use strict'
   if ($('contact-form')) {
     $('#contact-form').on('submit', function (e) {
@@ -60,9 +65,9 @@ if (indexdots) {
         data: dataString, 
         success: function (data) {
           $('.form-fields__button').attr('disabled', false).text('send email')
-          $('.error-block').empty().removeClass('visible');
+          $('.error-block').empty().removeClass('visible')
           if(data.errorMsg) {
-            $('.error-block').addClass('visible');
+            $('.error-block').addClass('visible')
             $.each(data.errorMsg, function (key, val) {
               $('<li class=\'error-block__item\'><i class=\'fa fa-warning\' aria-hidden=\'true\'></i>' 
                 + val.msg + '</li>').appendTo('.error-block')
---
+++ webpack.dev.config.js
@@ -0,0 +1,60 @@
+const path = require('path')
+
+module.exports = {
+  context: path.resolve(__dirname, 'public/src'),
+  entry: './js/script.js',
+  devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, './public/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [
+          path.resolve(__dirname, 'public/src/js')
+        ]
+      },
+      {
+        test: /.png|.jpg/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            publicPath: './dist/'
+          }
+        }
+      },
+      {
+        test: /\.(eot|ttf|svg|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            publicPath: '/dist/'
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              devtool: 'source-map'
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              devtool: 'source-map'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
